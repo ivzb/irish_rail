897d6661d306f148e57e1e7db741d6368fb27ce9
ivzb <izahariev96@gmail.com>
2020-04-25 12:44:08 +0300

    setup bottom navigation

--- app/src/main/java/com/ivzb/irish_rail/ui/main/MainActivity.kt
+++ app/src/main/java/com/ivzb/irish_rail/ui/main/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ivzb.irish_rail.ui.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ivzb.irish_rail.R
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
@@ -19,5 +23,10 @@ class MainActivity : DaggerAppCompatActivity() {
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
 
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
